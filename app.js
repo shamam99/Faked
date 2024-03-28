3d032dfb969ae450e654a76498a77d864976db92
shamam kafri <111068539+shamam99@users.noreply.github.com>
2024-03-28 05:29:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI)
     
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://faked.onrender.com'],
     credentials: true,
 }))
 
